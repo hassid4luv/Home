f6e5aa9707c32dd1f7f0e1ca0cc6428df5d83a4e
hassid4luv <hassi4luv@gmail.com>
2024-07-17 17:45:35 +0100

    Hotshy Home

---
+++ js/loadComponents.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//   function loadComponent(selector, url) {
+//       fetch(url)
+//           .then(response => response.text())
+//           .then(data => {
+//               document.querySelector(selector).innerHTML = data;
+//           })
+//           .catch(error => console.error('Error loading component:', error));
+//   }
+
+//   loadComponent('#navbar', 'navbar.html');
+//   loadComponent('#footer', 'footer.html');
+// });
